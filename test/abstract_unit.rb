b2a0a91a9cb634a2ba586f081366ebd8f8c0f076
tkadauke <tkadauke@60cb0372-8f51-4c35-923c-d3d8de73799b>
2009-01-09 20:57:47 +0000

    port to new dry_plugin_test_helper to get rid of warning
    git-svn-id: file:///var/local/svn/emma@7620 60cb0372-8f51-4c35-923c-d3d8de73799b

--- test/abstract_unit.rb
+++ test/abstract_unit.rb
@@ -3,5 +3,5 @@ require 'rubygems'
 require 'dry_plugin_test_helper'
 require 'mocha'
 
-PluginTestEnvironment.init_env(File.dirname(__FILE__))
+PluginTestEnvironment.initialize_environment(File.dirname(__FILE__))
 Article.acts_as_ordered
