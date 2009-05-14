0ca12d2f9f5eca86e730b97d845fe87fac9ebf10
Thomas Kadauke <tkadauke@imedo.de>
2009-05-14 16:10:06 +0200

    use fixed rails version

--- test/abstract_unit.rb
+++ test/abstract_unit.rb
@@ -3,5 +3,5 @@ require 'rubygems'
 require 'dry_plugin_test_helper'
 require 'mocha'
 
-PluginTestEnvironment.initialize_environment(File.dirname(__FILE__))
+PluginTestEnvironment.initialize_environment(File.dirname(__FILE__), :rails_version => '2.0.2')
 Article.acts_as_ordered
